0cefc337591e7effee06b996b6b0ec69254e1f69
avielman <avielman@gmail.com>
2018-06-15 18:45:32 -0600

    4. Se agrega lectura de archivo json.

--- index.js
+++ index.js
@@ -1,54 +1,29 @@
 var puerto = 81;
 
-var post = [
-      {
-        "userId": 1,
-        "id": 1,
-        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-      },
-      {
-        "userId": 1,
-        "id": 2,
-        "title": "qui est esse",
-        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-      },
-      {
-        "userId": 1,
-        "id": 3,
-        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-      },
-      {
-        "userId": 1,
-        "id": 4,
-        "title": "eum et est occaecati",
-        "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-      },
-      {
-        "userId": 1,
-        "id": 5,
-        "title": "nesciunt quas odio",
-        "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-      }
-    ];
+
+// consultando json
+var files = require('fs');
+console.log("\n *STARTING* \n");
+var pronosticos = files.readFileSync("pronostico.json");
+console.log("Ouput Content : \n" + pronosticos23
+);
+console.log("\n *EXIT* \n");
 
 
 // Express
 const express = require('express');
 const app = express();
 
-//app.use(express.static('public'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res) {    
     res.send("");
     console.log('corriendo en /');
 });
 
-post
-app.get('/posts', function(req, res) {
+app.get('/posts', function(req, res) {  
   console.log(post);
-  res.send(post);
+  res.send(pronosticos);
   console.log('corriendo en /posts');
 });
 
